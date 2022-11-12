ffab279b73058c883416870dab982d8002f6aaa3
GitDgon <dgon_mail@mail.ru>
2022-11-12 16:20:50 +0300

    Snipp создали

---
+++ Snippets/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from MainApp import views
+
+urlpatterns = [
+    path('', views.index_page, name='home'),
+    path('snippets/add', views.add_snippet_page, name='add_sn'),
+    path('snippets/list', views.snippets_page, name='list_sn'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
